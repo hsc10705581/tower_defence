ad1b3a29b4d0006452e7033242ef1771b005062b
hsc <hsc934936408@sjtu.edu.cn>
2019-10-08 19:42:04 +0800

    enable create object

---
+++ code/VR_tower/Assets/Scripts/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public Transform pointerDes;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateCube()
+    {
+        GameObject obj = Instantiate(cube, pointerDes.position, Quaternion.identity) as GameObject;
+    }
+}
